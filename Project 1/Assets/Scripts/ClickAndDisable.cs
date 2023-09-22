c7a9be234f8daf133a3050584470947974ab311f
abbottpiper <143674663+abbottpiper@users.noreply.github.com>
2023-09-22 15:29:35 -0500

    Working Flyswatter minigame prototype

---
+++ Project 1/Assets/Scripts/ClickAndDisable.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+public class ClickAndDisable : MonoBehaviour
+{
+    public float moveSpeed = 500f; 
+    private Vector3 targetPosition;
+
+    private void Start()
+    {
+        
+        MoveToRandomPosition();
+    }
+
+    private void Update()
+    {
+        
+        if (Input.GetMouseButtonDown(0))
+        {
+            
+            Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            
+            RaycastHit2D hit = Physics2D.Raycast(mousePosition, Vector2.zero);
+
+           
+            if (hit.collider != null)
+            {
+                
+                if (hit.collider.gameObject == gameObject)
+                {
+                    
+                    gameObject.SetActive(false);
+                }
+            }
+        }
+
+        
+        transform.position = Vector3.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
+
+       
+        if (Vector3.Distance(transform.position, targetPosition) < 0.1f)
+        {
+           
+            MoveToRandomPosition();
+        }
+    }
+
+    private void MoveToRandomPosition()
+    {
+        float randomX = Random.Range(-232f, 232f);
+        float randomY = Random.Range(-120f, 120f);
+        targetPosition = new Vector3(randomX, randomY, 0f);
+    }
+}
