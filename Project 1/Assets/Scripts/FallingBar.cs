eb365e3be778ed307a913e7af27ca4199232326d
abbottpiper <143674663+abbottpiper@users.noreply.github.com>
2023-09-28 11:24:37 -0500

    Watering bar function working

---
+++ Project 1/Assets/Scripts/FallingBar.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class FallingBar : MonoBehaviour
+{
+    public float fallSpeed = 2.0f; 
+    public float maxYPosition = 4.0f; 
+    public float minYPosition = -4.0f; 
+
+    private void Update()
+    {
+       
+        transform.Translate(Vector3.down * fallSpeed * Time.deltaTime);
+
+        
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, minYPosition, maxYPosition), transform.position.z);
+    }
+}
+
+public class PlayerController : MonoBehaviour
+{
+    public FallingBar fallingBar;
+    public float clickForce = 1.0f; 
+
+    private void Update()
+    {
+        
+        if (Input.GetMouseButtonDown(0))
+        {
+            
+            fallingBar.GetComponent<Rigidbody2D>().velocity = Vector2.up * clickForce;
+        }
+    }
+}
---
+++ Project 1/Assets/Scripts/WateringPlayerInput.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class WateringPlayerInput : MonoBehaviour
+{
+    public FallingBar fallingBar; 
+    public float clickForce = 5.0f; 
+    public float clickDuration = 0.1f; 
+
+    private float clickTimer = 0f;
+    private bool isClicking = false;
+
+    private void Update()
+    {
+      
+        if (Input.GetMouseButtonDown(0))
+        {
+           
+            clickTimer = 0f;
+            isClicking = true;
+        }
+
+        if (isClicking)
+        {
+          
+            fallingBar.GetComponent<Rigidbody2D>().velocity = Vector2.up * clickForce;
+
+            
+            clickTimer += Time.deltaTime;
+
+            
+            if (clickTimer >= clickDuration)
+            {
+                isClicking = false;
+            }
+        }
+    }
+}
