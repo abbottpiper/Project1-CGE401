d57fd5ffdcdcd44b9a2f02849a6f3f3165c71174
abbottpiper <143674663+abbottpiper@users.noreply.github.com>
2023-09-28 14:48:26 -0500

    watering working

---
+++ Project 1/Assets/Scripts/ProgressBar.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ProgressBar : MonoBehaviour
+{
+    public FallingBar fallingBar; 
+    public Slider progressBar; 
+    public float minYPosition = -7.0f; 
+    public float maxYPosition = 7.0f; 
+    public float incrementSpeed = 0.1f; 
+
+    private void Update()
+    {
+        
+        float yPosition = fallingBar.transform.position.y;
+        if (yPosition >= minYPosition && yPosition <= maxYPosition)
+        {
+            
+            progressBar.value += incrementSpeed * Time.deltaTime;
+
+            
+            progressBar.value = Mathf.Clamp01(progressBar.value);
+        }
+    }
+}
