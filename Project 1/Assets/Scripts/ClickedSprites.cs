aa9ad2d537eec0acb2ceeb066884eb155d026f92
Quetzalberry <denverheneghan@gmail.com>
2023-09-29 17:51:01 -0500

    buttons

--- Project 1/Assets/Scripts/ClickedSprites.cs
+++ Project 1/Assets/Scripts/ClickedSprites.cs
@@ -6,19 +6,22 @@ using UnityEngine.Events;
 public class ClickedSprites : MonoBehaviour
 {
     public bool mouseClick;
+    int totalCount = 0;
+    int countApple = 1;
+    int orderOneDone = 4;
 
     private void Update()
     {
+        if (mouseClick == true)
+        {
+            totalCount += countApple;
 
+            mouseClick = false;
+        }
     }
 
     private void OnMouseDown()
     {
         mouseClick = true;
-    }
-
-    private void OnMouseUp()
-    {
-        mouseClick = false;
     }
 }
--- Project 1/Assets/Scripts/TimerScript.cs
+++ Project 1/Assets/Scripts/TimerScript.cs
@@ -4,23 +4,14 @@ using UnityEngine;
 
 public class TimerScript : MonoBehaviour
 {
-    ClickedSprites clickedSpritesScript;
-
-    public GameObject PlantSecondStage;
-
     private void Start()
     {
-       // clickedSpritesScript = PlantSecondStage.GetComponent<ClickedSprites>();
 
-        //PlantSecondStage.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        //if (clickedSpritesScript.mouseClick == true)
-       // {
-          //  PlantSecondStage.SetActive(true);
-       // }
+
     }
 }
