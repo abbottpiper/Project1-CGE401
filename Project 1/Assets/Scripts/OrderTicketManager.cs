3d039da29ff4e169f0de9d0234e063844bdd5c25
abbottpiper <143674663+abbottpiper@users.noreply.github.com>
2023-09-29 02:48:46 -0500

    functioning ticket enabler with placeholders

---
+++ Project 1/Assets/Scripts/OrderTicketManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OrderTicketManager : MonoBehaviour
+{
+    public Image[] orderTickets; // An array of your order ticket images.
+    public float enableInterval = 3f; // Time interval between enabling order tickets.
+    private int currentIndex = 0; // Index of the currently enabled order ticket.
+
+    private void Start()
+    {
+        StartCoroutine(EnableOrderTickets());
+    }
+
+    private IEnumerator EnableOrderTickets()
+    {
+        while (currentIndex < orderTickets.Length)
+        {
+            // Enable the current order ticket.
+            orderTickets[currentIndex].enabled = true;
+
+            // Wait for the specified enable interval.
+            yield return new WaitForSeconds(enableInterval);
+
+            // Move to the next order ticket in the array.
+            currentIndex++;
+        }
+    }
+}
