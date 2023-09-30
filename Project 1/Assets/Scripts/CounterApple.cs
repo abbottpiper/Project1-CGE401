d6e712bfd3db6fefb05afec0c4eef81a6dc1c7e9
Quetzalberry <denverheneghan@gmail.com>
2023-09-30 13:46:26 -0500

    Setting Values

---
+++ Project 1/Assets/Scripts/CounterApple.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CounterApple : MonoBehaviour
+{
+    public int totalCount;
+
+    public int totalFood;
+
+    public int appleCount = 1;
+
+    private void Start()
+    {
+        totalCount = 0;
+        totalFood = 0;
+    }
+
+    public void AddToScore()
+    {
+        totalCount += 1;
+        totalFood += appleCount;
+    }
+}
---
+++ Project 1/Assets/Scripts/CounterOrange.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CounterOrange : MonoBehaviour
+{
+    public int totalCount;
+
+    public int totalFood;
+
+    public int orangeCount = 2;
+
+    private void Start()
+    {
+        totalCount = 0;
+        totalFood = 0;
+    }
+
+    public void AddToScore()
+    {
+        totalCount += 1;
+        totalFood += orangeCount;
+    }
+}
