a465c8f9d23d48f1bb1cdfdce937d0a9afbe156b
Quetzalberry <denverheneghan@gmail.com>
2023-09-30 16:18:19 -0500

    Win Loss Conditions

--- Project 1/Assets/Scripts/CountOrders.cs
+++ Project 1/Assets/Scripts/CountOrders.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class CounterApple : MonoBehaviour
+public class CountOrders : MonoBehaviour
 {
     public int totalCount;
 
     public int totalFood;
 
     public int appleCount = 1;
+    public int orangeCount = 2;
 
     private void Start()
     {
@@ -17,9 +18,15 @@ public class CounterApple : MonoBehaviour
         totalFood = 0;
     }
 
-    public void AddToScore()
+    public void AddAppleToScore()
     {
         totalCount += 1;
         totalFood += appleCount;
+    }
+
+    public void AddOrangeToScore()
+    {
+        totalCount += 1;
+        totalFood += orangeCount;
     }
 }
---
+++ Project 1/Assets/Scripts/WinAndLoseConditions.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WinAndLoseConditions : MonoBehaviour
+{
+    private CountOrders countOrdersScript;
+    private HealthSystem healthSystemScript;
+
+    public Text Order;
+
+    public float time = 10.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Order = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (time > 0)
+        {
+            time -= Time.deltaTime;
+        }
+
+        if (time <= 0)
+        {
+            healthSystemScript.TakeDamage();
+            time = 10.0f;
+        }
+
+        OrderOne();
+    }
+
+    public void OrderOne()
+    {
+        if (countOrdersScript.totalCount == 4)
+        {
+            if (countOrdersScript.totalFood == 4)
+            {
+                Order.text = "Correct! Great Job!";
+                time = 10.0f;
+            }
+            else if(countOrdersScript.totalFood != 4)
+            {
+                healthSystemScript.TakeDamage();
+                Order.text = "Incorrect, try again.";
+            }
+        }
+    }
+
+    public void OrderTwo()
+    {
+
+    }
+}
