8edaca23e0a9378031bc4f2c15572e525095a74b
Quetzalberry <denverheneghan@gmail.com>
2023-10-04 18:52:51 -0500

    Wrong and Correct Orders

--- Project 1/Assets/Scripts/WinAndLoseConditions.cs
+++ Project 1/Assets/Scripts/WinAndLoseConditions.cs
@@ -8,44 +8,49 @@ public class WinAndLoseConditions : MonoBehaviour
     public CountOrders countOrdersScript;
     public HealthSystem healthSystemScript;
 
-    public Text Order;
+   // public float time = 15.0f;
 
-    public float time = 15.0f;
+    public GameObject CorrectOrderText;
+    public GameObject WrongOrderText;
 
     // Start is called before the first frame update
     void Start()
     {
-        Order = GetComponent<Text>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (time > 0)
-        {
-            time -= Time.deltaTime;
-        }
-        else
-        {
-            time = 15.0f;
-            healthSystemScript.TakeDamage();
-        }
+       // if (time > 0)
+        //{
+            //time -= Time.deltaTime;
+        //}
+        //else
+        //{
+           // time = 15.0f;
+          //  healthSystemScript.TakeDamage();
+        //}
     }
 
     public void OrderOne()
     {
         if (countOrdersScript.totalFood == 4)
         {
-            //Order.text = "Correct! Great Job!";
-            time = 15.0f;
+            CorrectOrderText.SetActive(true);
+
+           // time = 15.0f;
 
             countOrdersScript.totalCount = 0;
             countOrdersScript.totalFood = 0;
+
+            Destroy(GameObject.FindWithTag("OrderOne"));
         }
         else
         {
             healthSystemScript.TakeDamage();
-            //Order.text = "Incorrect, try again.";
+
+            WrongOrderText.SetActive(true);
 
             countOrdersScript.totalCount = 0;
             countOrdersScript.totalFood = 0;
@@ -56,16 +61,20 @@ public class WinAndLoseConditions : MonoBehaviour
     {
         if (countOrdersScript.totalFood == 10)
         {
-            //Order.text = "Correct! Great Job!";
-            time = 15.0f;
+            CorrectOrderText.SetActive(true);
+
+           // time = 15.0f;
 
             countOrdersScript.totalCount = 0;
             countOrdersScript.totalFood = 0;
+
+            Destroy(GameObject.FindWithTag("OrderTwo"));
         }
         else
         {
             healthSystemScript.TakeDamage();
-            //Order.text = "Incorrect, try again.";
+
+            WrongOrderText.SetActive(true);
 
             countOrdersScript.totalCount = 0;
             countOrdersScript.totalFood = 0;
@@ -76,16 +85,20 @@ public class WinAndLoseConditions : MonoBehaviour
     {
         if (countOrdersScript.totalFood == 9)
         {
-            //Order.text = "Correct! Great Job!";
-            time = 15.0f;
+            CorrectOrderText.SetActive(true);
+
+           // time = 15.0f;
 
             countOrdersScript.totalCount = 0;
             countOrdersScript.totalFood = 0;
+
+            Destroy(GameObject.FindWithTag("OrderThree"));
         }
         else
         {
             healthSystemScript.TakeDamage();
-            //Order.text = "Incorrect, try again.";
+
+            WrongOrderText.SetActive(true);
 
             countOrdersScript.totalCount = 0;
             countOrdersScript.totalFood = 0;
