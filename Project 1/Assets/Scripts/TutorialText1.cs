8ab393f4f4c8ddf46007c414414054ea9bdefff6
abbottpiper <143674663+abbottpiper@users.noreply.github.com>
2023-10-05 15:37:50 -0500

    text work

---
+++ Project 1/Assets/Scripts/TutorialText1.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TutorialText1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+		{
+			gameObject.SetActive(false);
+		}
+    }
+}
