4407579e7061604fbe236b3c0e43d168bc11d07f
abbottpiper <143674663+abbottpiper@users.noreply.github.com>
2023-10-06 18:35:11 -0500

    fly

--- Project 1/Assets/Scripts/FlyController.cs
+++ Project 1/Assets/Scripts/FlyController.cs
@@ -6,17 +6,20 @@ public class FlyController : MonoBehaviour
 
     void Start()
     {
-        // Find the GameObject with the name "Fly" in the scene
+       
         flyObject = GameObject.Find("Fly");
 
-        // Ensure the fly object is initially disabled
+        
         if (flyObject != null)
         {
             flyObject.SetActive(false);
         }
-       
+        else
+        {
+            Debug.LogError("Fly object not found in the scene.");
+        }
 
-        // Invoke the EnableFlyObject method after 40 seconds and repeat every 60 seconds
+        
         InvokeRepeating("EnableFlyObject", 40f, 60f);
     }
 
@@ -24,7 +27,7 @@ public class FlyController : MonoBehaviour
     {
         if (flyObject != null)
         {
-            // Enable the fly object
+           
             flyObject.SetActive(true);
         }
     }
