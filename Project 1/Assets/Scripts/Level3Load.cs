6be3cf5af81be06a425598d6e2df90d95e9b7a12
abbottpiper <143674663+abbottpiper@users.noreply.github.com>
2023-10-08 14:02:17 -0500

    nextbutton function working

---
+++ Project 1/Assets/Scripts/Level3Load.cs
@@ -0,0 +1,25 @@
+﻿/*
+Piper Abbott-Phillips
+Level3Load.cs
+Project 2
+Code controls button function and scene loading from level2 to level3
+*/
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+public class Level3Load : MonoBehaviour
+{
+	public Button yourButton;
+
+	void Start()
+	{
+		Button btn = yourButton.GetComponent<Button>();
+		btn.onClick.AddListener(TaskOnClick);
+	}
+
+	void TaskOnClick()
+	{
+		SceneManager.LoadScene("Level3", LoadSceneMode.Single);
+	}
+}
