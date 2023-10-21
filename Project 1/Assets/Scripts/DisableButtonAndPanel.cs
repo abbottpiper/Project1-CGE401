26f30276a763c72a0c07d9b285ce9924f599d6f0
abbottpiper <143674663+abbottpiper@users.noreply.github.com>
2023-10-20 21:25:04 -0500

    Panel system placeholders up and running

---
+++ Project 1/Assets/Scripts/DisableButtonAndPanel.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class DisableButtonAndPanel : MonoBehaviour
+{
+    public Button button;
+    public GameObject panel;
+
+    private void Start()
+    {
+        // Get a reference to the Button component
+        button = GetComponent<Button>();
+
+        // Attach a listener to the button's click event
+        button.onClick.AddListener(DisableButtonAndPanelOnClick);
+    }
+
+    private void DisableButtonAndPanelOnClick()
+    {
+        // Disable the button
+        button.interactable = false;
+
+        // Disable the panel
+        panel.SetActive(false);
+    }
+}
---
+++ Project 1/Assets/Scripts/TextRevealer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextRevealer : MonoBehaviour
+{
+    public Text textComponent;
+    public float revealSpeed = 0.1f;
+
+    private string fullText;
+    private string currentText = "";
+
+    private void Start()
+    {
+        textComponent = GetComponent<Text>();
+        fullText = textComponent.text;
+        textComponent.text = "";
+        StartCoroutine(RevealText());
+    }
+
+    private IEnumerator RevealText()
+    {
+        for (int i = 0; i < fullText.Length; i++)
+        {
+            currentText += fullText[i];
+            textComponent.text = currentText;
+            yield return new WaitForSeconds(revealSpeed);
+        }
+    }
+}
